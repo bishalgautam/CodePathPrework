3302dc34f867d06c1a589ee655e07344b748abdd
bishalgautam <bishal.htd04@gmail.com>
2017-01-05 14:26:11 -0500

    the basic are done !!

---
+++ tipcalc.php
@@ -0,0 +1,24 @@
+<? php
+
+?>
+<html>  
+<head>
+	<title>My Tip Calcultor </title>
+</head>
+<body>
+<h1> TIP CALCULATOR </h1>
+ <form> 
+ <p> Bill Subtotal : $ <input> </p>
+
+ <p> Tip Percentage : </p>
+
+ <input type="radio" name="answer1" value="6">
+
+
+
+ </form>
+
+</body>
+</html>
+
+
